1891fc3ea4a5e3fd53bf82c37364f80288436ec4
Flavia Rafaela Moura <flaviarafaelamoura1@gmail.com>
2023-05-17 20:13:59 -0300

    chore: Change data type of dateOfBirth to DATEONLY

--- activities/API/src/models/customers.js
+++ activities/API/src/models/customers.js
@@ -4,7 +4,7 @@ const DataTypes = require('sequelize');
 const customers = database.define('customers', {
   name: DataTypes.STRING,
   gender: DataTypes.STRING,
-  dateOfBirth: DataTypes.DATE,
+  dateOfBirth: DataTypes.DATEONLY,
   age: DataTypes.STRING,
   city: DataTypes.STRING
 });
